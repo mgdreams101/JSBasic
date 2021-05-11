a8e5967206e4569515e779b8f7766d98fdf81a07
Miro global <mgdreams101@gmail.com>
2021-05-11 15:11:33 +0300

    js function to color odd and even numbers and list them as bullet points

--- leapYear.js
+++
@@ -1,7 +0,0 @@
-function leapYear(year){
-    let leap = (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
-
-    console.log(leap ? `Year ${year} IS a lear year!` : `Year ${year} is NOT a lear year!`);
-}
-
-leapYear(2024);
