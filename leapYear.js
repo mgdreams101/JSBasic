ddb207e6cb292c4fed59a9d1ffd59a281130e83b
Miro global <mgdreams101@gmail.com>
2021-05-11 12:58:52 +0300

    formatted return message

--- leapYear.js
+++ leapYear.js
@@ -1,7 +1,7 @@
 function leapYear(year){
     let leap = (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
 
-    console.log(leap);
+    console.log(leap ? `Year ${year} IS a lear year!` : `Year ${year} is NOT a lear year!`);
 }
 
 leapYear(2024);
